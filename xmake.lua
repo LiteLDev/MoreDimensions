2248b752f10d695be9b555688d8109e1fecb4fda
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2024-03-13 18:15:31 -0700

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-add_requires("levilamina 0.8.3")
+add_requires("levilamina 0.9.3")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
