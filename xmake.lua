e58f4bb9e2044f63b5754656b91fd0ec7859e28d
ShrBox <shrbox@outlook.com>
2026-09-12 22:32:19 +0800

    chore: remove useless dependency

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,6 @@ add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_requires("levilamina 26.20.*", {configs = {target_type = get_config("target_type")}})
 
 add_requires("levibuildscript")
-add_requires("preloader 1.15.7")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
